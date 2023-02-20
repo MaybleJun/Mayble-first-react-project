85218b8482aa62588abcd542771df57dfc23466a
Mayble Jun <mayblejul@gmail.com>
2023-02-21 00:37:03 +0400

    Add Feature-Sliced Design     (Architectural methodology for frontend projects)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,12 @@
 import React, {Suspense} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
 import './styles/index.scss';
-
-import {MainPageAsync} from "pages/MainPage/ui/MainPage.async";
-
 import {classNames} from "shared/lib/classNames/classNames";
-import { useTheme } from 'app/providers/ThemeProvider';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
+import {useTheme} from "app/providers/ThemeProvider";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
+
+
 
 
 const App = () => {
