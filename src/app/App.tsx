f332128e1b30aa62964f655859d22172513f1472
Mayble Jun <mayblejul@gmail.com>
2023-02-22 01:32:32 +0400

    Add AppRouter, config for router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, {Suspense} from 'react';
+import {Link, Route, Routes} from 'react-router-dom';
 import './styles/index.scss';
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
-import {AppRouter} from "app/providers/router";
+import { AppRouter } from './providers/router';
 
 
 
